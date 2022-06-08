fb0539a9b812ba45fdaeed2492603056e8393903
stas224stas <stas224stas@gmai.com>
2022-06-08 14:40:48 +0300

    commit 7

--- hook/src/index.ts
+++ hook/src/index.ts
@@ -12,12 +12,16 @@ const textToCommand = (texts: string[]) => {
     let restarts = ['заново', 'новая игра', 'с начала', 'перезап', 'снова', 'по новой'];
     let guessedright = ['угадал','угадано','выиграл','отгадал','отгадано', 'выиграно'];
     let guessedwrong = ['не угадал','не угадано','не выиграл','не отгадал','не отгадано', 'не выиграно','неугаданно'];
+    let close = ['закр','закрой помощь','скрой помощь','закрой справку', 'скрой справку', 'закрой мануал', 'скрой мануал', 'убери помощь', 'убери справку', 'убери мануал', 'поня', 'ясно', 'понял'];
 
     for (let dir of changeword) {
         if (text.includes(dir)) return {type: 'changeword'};
     }
     for (let dir of changemode) {
         if (text.includes(dir)) return {type: 'changemode'};
+    }
+    for (let dir of close){
+        if (text.includes(dir)) return {type: 'close'};
     }
     for (let help of helps){
         if (text.includes(help)) return {type: 'help'};
@@ -41,26 +45,27 @@ function* script(r: SberRequest) {
     const rsp = r.buildRsp();
     let changewordPhrases = ['Сделано!','Готово!','Новое слово на экране!'];
     let changemodePhrases = ['Сделано!','Готово!','Новый режим включён!'];
-    let maleFailPhrases = ['Я пока не выучил эту команду', 'Расшифровка этого займет несколько часов',
-        'Над этим мне нужно подумать', 'Разработчики работают над добавлением этой команды',
-        'Скоро я пойму, что это значит'];
-    let femaleFailPhrases = ['Я пока не выучила эту команду', 'Расшифровка этого займет несколько часов',
-        'Над этим мне нужно подумать', 'Разработчики работают над добавлением этой команды',
-        'Скоро я пойму, что это значит'];
+    let maleofficialFailPhrases = ['Я пока не выучил эту команду. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет', 'Расшифровка этого займет несколько часов. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет',
+        'Над этим мне нужно подумать. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет', 'Разработчики работают над добавлением этой команды. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет',
+        'Скоро я пойму, что это значит. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет'];
+    let maleno_officialFailPhrases = ['Я пока не выучил эту команду. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет', 'Расшифровка этого займет несколько часов. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет',
+        'Над этим мне нужно подумать. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет', 'Разработчики работают над добавлением этой команды. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет',
+        'Скоро я пойму, что это значит. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет'];
+    let femaleofficialFailPhrases = ['Я пока не выучила эту команду. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет', 'Расшифровка этого займет несколько часов. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет',
+        'Над этим мне нужно подумать. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет', 'Разработчики работают над добавлением этой команды. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет',
+        'Скоро я пойму, что это значит. Лучше скажите «помощь» или нажмите на одноимённую кнопку, возможно, это чем-то поможет'];
+    let femaleno_officialFailPhrases = ['Я пока не выучила эту команду. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет', 'Расшифровка этого займет несколько часов. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет',
+        'Над этим мне нужно подумать. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет', 'Разработчики работают над добавлением этой команды. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет',
+        'Скоро я пойму, что это значит. Лучше скажи «помощь» или нажми на одноимённую кнопку, возможно, это чем-то поможет'];
     let GuessedRightPhrases = ['Так держать!','Превосходно!','',''];
-    let GuessedWrongPhrases = ['Ничего страшного!','Всё хорошо!','',''];
-    let officialGreets = ['Добро пожаловать в приложение «Слова для Крокодила»! Суть игры - объяснить слово с экрана, используя только мимику, жесты и движения. Я помогу вам подобрать интересные слова. Для этого выберите режим и нажмите или скажите «Новое слово».'];
-    let unOfficialGreets = ['Привет', 'Привет-привет', 'Салют'];
+    let GuessedWrongPhrases = ['Ничего страшного!','Повезёт в следующий раз!','',''];
+    let officialGreets = ['Здравствуйте!'];
+    let unOfficialGreets = ['Салют!'];
     let { gender, appeal } = r.body.payload.character;
 
     let phrase;
-    if (appeal === 'official') {
-        let phraseIndex = Math.floor(Math.random() * officialGreets.length);
-        phrase = officialGreets[phraseIndex];
-    }else {
-        let phraseIndex = Math.floor(Math.random() * unOfficialGreets.length);
-        phrase = unOfficialGreets[phraseIndex];
-    }
+    let phraseIndex = Math.floor(Math.random() * officialGreets.length);
+    phrase = officialGreets[phraseIndex] + ` Добро пожаловать в приложение «Слова для Крокодила»! Данный смартап предназначен для всем известной игры, где ${(appeal === 'official' ? 'вам' : 'тебе')} нужно объяснить слово с экрана, используя только мимику, жесты и движения. Здесь можно попросить новое слово, поменять режим и даже вести счёт отгаданных слов!`;
     rsp.msg = phrase;
     rsp.data = {type: 'init'};
     yield rsp;
@@ -92,14 +97,31 @@ function* script(r: SberRequest) {
                 rsp.data = command;
             }
             else if (command.type === 'fail') {
+                let { gender, appeal } = r.body.payload.character;
+                console.log(gender, appeal);
                 if (gender == 'male') {
-                    let phraseIndex = Math.floor(Math.random() * maleFailPhrases.length);
-                    phrase = maleFailPhrases[phraseIndex];
-                }else{
-                    let phraseIndex = Math.floor(Math.random() * femaleFailPhrases.length);
-                    phrase = femaleFailPhrases[phraseIndex];
+                    if (appeal == 'official') {
+                        let phraseIndex = Math.floor(Math.random() * maleofficialFailPhrases.length);
+                        phrase = maleofficialFailPhrases[phraseIndex];
+                    }
+                    else {
+                        let phraseIndex = Math.floor(Math.random() * maleno_officialFailPhrases.length);
+                        phrase = maleno_officialFailPhrases[phraseIndex];
+                    }
+                }
+                else {
+                    if (appeal == 'official') {
+                        let phraseIndex = Math.floor(Math.random() * femaleofficialFailPhrases.length);
+                        phrase = femaleofficialFailPhrases[phraseIndex];
+                    }
+                    else {
+                        let phraseIndex = Math.floor(Math.random() * femaleno_officialFailPhrases.length);
+                        phrase = femaleno_officialFailPhrases[phraseIndex];
+                    }
                 }
                 rsp.msg = phrase;
+                rsp.data = command;
+            }else if (command.type === 'close') {
                 rsp.data = command;
             }else if (command.type === 'help') {
                 rsp.data = command;
